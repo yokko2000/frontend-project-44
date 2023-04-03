b96b16c6c87c737a9038b3602d77442c4cb41d89
Maksim Komiachko <yokko1990@yandex.ru>
2023-04-03 23:55:53 +0300

    some fix

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,16 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-
 export function welcomeUser() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 }
-
 const ROUNDS_COUNT = 3;
-
 export function runGame (getQuestionAndAnswer, gameDescription) {
   console.log('Welcome to the Brain Games!');
   console.log(gameDescription);
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-
   for (let i = 0; i < ROUNDS_COUNT; i += 1) {
     const { question, correctAnswer } = getQuestionAndAnswer();
     console.log(`Question: ${question}`);
@@ -28,6 +24,5 @@ export function runGame (getQuestionAndAnswer, gameDescription) {
       return;
     }
   }
-
   console.log(`Congratulations, ${name}!`);
 };
