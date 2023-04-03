86a925567b21e64b9d0c18c009709922a184080c
Maksim Komiachko <yokko1990@yandex.ru>
2023-04-03 23:49:17 +0300

    Done all steps

---
+++ bin/brain-prime.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import { runGame } from '../src/cli.js';
+import { getRandomInt } from '../src/utils.js';
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+//функция генерации вопроса и ответа
+function getQuestionAndAnswer() {
+  const number = getRandomInt(1,100);
+  const question = String(number);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return { question, correctAnswer };
+}
+//функция проверяет простое ли число
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+  runGame(getQuestionAndAnswer, gameDescription);
